136c8ce462afa67394fa63efdcc4de780faace82
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-10-24 01:14:09 +0300

    Update pyDecor.py

--- pyDecor.py
+++ pyDecor.py
@@ -1,11 +1,6 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> def repeat(n):
+ def repeat(n):
 	def fake_func(true_func):
 		def repeater(argument):
-			if n == 0:
-				print(argument)
-			return argument
 			for counter in range(n):
 				result = true_func(argument)
 				argument = result
