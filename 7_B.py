8130569b9e7b8ad43e2ceff527d37c7eb8874599
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-11-14 09:45:13 +0300

    Update 7_B.py

--- 7_B.py
+++ 7_B.py
@@ -1,6 +1,4 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> #Используйте вторую фунцию
+#Используйте вторую фунцию
 
 import math
 import random
