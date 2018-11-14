41e65972e46aa6b2ca52c09521286a0a6280de9e
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-11-14 09:39:50 +0300

    Add files via upload

---
+++ 7_A.py
@@ -0,0 +1,9 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def gcd(a,b):
+    while b:
+        a %= b
+        c = a
+        a = b
+        b = c
+    return a
