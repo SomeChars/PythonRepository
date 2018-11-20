155cefc4a73532f7d2c84a443bd16100dc67c174
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-11-21 00:50:26 +0300

    Update 7_C.py

--- 7_C.py
+++ 7_C.py
@@ -1,20 +1,18 @@
+#Способ короче в голову не пришёл(
+
 def str_base(number, base):
-        i = 0
-        func_number = number
+        i = 1
         sum = ""
-        while number % base**i != number:
+        while number > base**i:
             i += 1
         i -= 1
-        while func_number > base and i >= 0:
-            a = int(func_number / base**i)
-            func_number -= a*(base**i)
-            i -= 1
+        while i >= 0:
+            a = int(number / base**i)
+            number -= a*(base**i)
             if a < 10:
-                sum += str(a)
-                if i == 0:
-                    sum += str(func_number)
+                    sum += str(a)
             else:
-                sum += chr(65 + a - 10)
                 if i == 0:
-                    sum += chr(65 + func_number - 10)
+                    sum += chr(65 + a - 10)
+            i -= 1
         return sum
