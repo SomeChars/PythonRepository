0bc1d0034464046d0c3b11453f771f7e0e0fa310
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-11-27 19:46:23 +0300

    Add files via upload

---
+++ Gauss.py
@@ -0,0 +1,53 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import copy  # to copy list of lists
+
+m = [
+    [6,5,4,3,1],
+    [1,1,1,1,1],
+    [2,1,1,1,2],
+    [2,2,2,1,1],
+    [1,1,1,1,2]
+]
+
+x = [44,15,21,21,20]
+
+def forward(a,b):
+    flag = 0
+    for i in range (a.__len__() - 1):
+        for j in range (i + 1,a.__len__()):
+            c = a
+            d = b
+            if a[j][i] == 0:
+                continue
+            diff = a[j][i] / a[i][i]
+            d[i] *= diff
+            b[j] -= d[i]
+            d[i] /= diff
+            for k in range (a.__len__()):
+                c[i][k] *= diff
+                a[j][k] -= c[i][k]
+                c[i][k] /= diff
+    return a,b
+
+
+def backward(a,b):
+    a[a.__len__() - 1][a.__len__() - 1] = b[b.__len__() - 1] / a[a.__len__() - 1][a.__len__() - 1]
+    for i in range (1,a.__len__()):
+        for j in range (a.__len__()):
+            if j == (a.__len__() - i - 1):
+                continue
+            a[a.__len__() - i - 1][j] *= a[a.__len__() - i][j]
+            b[a.__len__() - i - 1] -= a[a.__len__() - i - 1][j]
+            if a[a.__len__() - i][j] != 0:
+                a[a.__len__() - i - 1][j] = a[a.__len__() - i][j]
+        a[a.__len__() - i - 1][a.__len__() - i - 1] = b[b.__len__() - i - 1] / a[a.__len__() - i - 1][a.__len__() - i - 1]
+    for k in range (a.__len__()):
+        b[k] = a[k][k]
+    return b
+
+def Gauss(a,b):
+    c,d = forward(a,b)
+    return(backward(c,d))
+
+print(Gauss(m,x))
