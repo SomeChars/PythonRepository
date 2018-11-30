1dce243d0e7661112a8a2e31444377665abcc1d2
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-11-30 21:10:02 +0300

    Update gcd_nji_jit.py

--- gcd_nji_jit.py
+++ gcd_nji_jit.py
@@ -1,6 +1,4 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> from numba import njit,jit
+from numba import njit,jit
 
 def gcd(a,b):
     while b:
