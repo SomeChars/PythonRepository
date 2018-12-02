25db7750ec529458876edccea1a63250c73a2c63
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-12-02 14:07:24 +0300

    Update Gauss.py

--- Gauss.py
+++ Gauss.py
@@ -8,11 +8,17 @@ m = [
     [1,1,1,1,2]
 ]
 
-x = [44,15,21,21,20]
+x = [45,15,21,21,20]
+
+len = m.__len__()
+lenb = x.__len__()
 
 def forward(a,b):
-    for i in range (a.__len__() - 1):
-        for j in range (i + 1,a.__len__()):
+    for i in range (len - 1):
+        if a[i][i] == 0:
+            print("Некорректный ввод")
+            break
+        for j in range (i + 1,len):
             c = a
             d = b
             if a[j][i] == 0:
@@ -21,30 +27,25 @@ def forward(a,b):
             d[i] *= diff
             b[j] -= d[i]
             d[i] /= diff
-            for k in range (a.__len__()):
+            for k in range (len):
                 c[i][k] *= diff
                 a[j][k] -= c[i][k]
                 c[i][k] /= diff
     return a,b
 
-
 def backward(a,b):
     is_ok = 1
-    if a[a.__len__() - 1][a.__len__() - 1] != 0:
-        a[a.__len__() - 1][a.__len__() - 1] = b[b.__len__() - 1] / a[a.__len__() - 1][a.__len__() - 1]
+    if a[len - 1][len - 1] != 0:
+        a[len - 1][len - 1] = b[lenb - 1] / a[len - 1][len - 1]
     else:
         print("Некорректный ввод")
         is_ok = 0
-    for i in range (1,a.__len__()):
-        for j in range (a.__len__()):
-            if j == (a.__len__() - i - 1):
-                continue
-            a[a.__len__() - i - 1][j] *= a[a.__len__() - i][j]
-            b[a.__len__() - i - 1] -= a[a.__len__() - i - 1][j]
-            if a[a.__len__() - i][j] != 0:
-                a[a.__len__() - i - 1][j] = a[a.__len__() - i][j]
-        a[a.__len__() - i - 1][a.__len__() - i - 1] = b[b.__len__() - i - 1] / a[a.__len__() - i - 1][a.__len__() - i - 1]
-    for k in range (a.__len__()):
+    for i in range (1,len):
+        for j in range (len - i,len):
+            b[lenb - i - 1] -= a[len - i - 1][j] * a[len - i][j]
+            a[len - i - 1][j] = a[len - i][j]
+        a[len - i - 1][len - i - 1] = b[lenb - i - 1] / a[len - i - 1][len - i - 1]
+    for k in range (len):
         b[k] = a[k][k]
     if is_ok:
         return b
@@ -52,3 +53,5 @@ def backward(a,b):
 def Gauss(a,b):
     c,d = forward(a,b)
     return(backward(c,d))
+
+print(Gauss(m,x))
