3d9f64fbde3bbad19267bf84fa4f2db1bd68fd2b
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-12-05 11:32:16 +0300

    Update quaternions.py

--- quaternions.py
+++ quaternions.py
@@ -1,6 +1,4 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import math
+import math
 
 class Quaternion:
     def __init__(self,free_coefficient,*args):
