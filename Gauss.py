1bb420a3d5fb5126fdfc30054627a8ac1a39a685
SomeChars <43200522+SomeChars@users.noreply.github.com>
2018-12-05 22:19:30 +0300

    Update Gauss.py

--- Gauss.py
+++ Gauss.py
@@ -9,11 +9,11 @@ m = [
 x = [45,15,21,21,20]
 
 def forward(a,b):
-    for i in range (m.__len__() - 1):
+    for i in range (len(a) - 1):
         if a[i][i] == 0:
             print("Некорректный ввод")
             break
-        for j in range (i + 1,m.__len__()):
+        for j in range (i + 1,len(a)):
             c = a
             d = b
             if a[j][i] == 0:
@@ -22,7 +22,7 @@ def forward(a,b):
             d[i] *= diff
             b[j] -= d[i]
             d[i] /= diff
-            for k in range (m.__len__()):
+            for k in range (len(a)):
                 c[i][k] *= diff
                 a[j][k] -= c[i][k]
                 c[i][k] /= diff
@@ -30,17 +30,17 @@ def forward(a,b):
 
 def backward(a,b):
     is_ok = 1
-    if a[m.__len__() - 1][m.__len__() - 1] != 0:
-        a[m.__len__() - 1][m.__len__() - 1] = b[x.__len__() - 1] / a[m.__len__() - 1][m.__len__() - 1]
+    if a[len(a) - 1][len(a) - 1] != 0:
+        a[len(a) - 1][len(a) - 1] = b[len(a) - 1] / a[len(a) - 1][len(a) - 1]
     else:
         print("Некорректный ввод")
         is_ok = 0
-    for i in range (1,m.__len__()):
-        for j in range (m.__len__() - i,m.__len__()):
-            b[x.__len__() - i - 1] -= a[m.__len__() - i - 1][j] * a[m.__len__() - i][j]
-            a[m.__len__() - i - 1][j] = a[m.__len__() - i][j]
-        a[m.__len__() - i - 1][m.__len__() - i - 1] = b[x.__len__() - i - 1] / a[m.__len__() - i - 1][m.__len__() - i - 1]
-    for k in range (m.__len__()):
+    for i in range (1,len(a)):
+        for j in range (len(a) - i,len(a)):
+            b[len(a) - i - 1] -= a[len(a) - i - 1][j] * a[len(a) - i][j]
+            a[len(a) - i - 1][j] = a[len(a) - i][j]
+        a[len(a) - i - 1][len(a) - i - 1] = b[len(a) - i - 1] / a[len(a) - i - 1][len(a) - i - 1]
+    for k in range (len(a)):
         b[k] = a[k][k]
     if is_ok == 1:
         return b
